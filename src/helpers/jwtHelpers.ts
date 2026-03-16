b12ec2ba54df9d4781ac5c7b754a9f743ca2b278
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-03-17 01:42:29 +0600

    fix:jwtHelpers

--- src/helpers/jwtHelpers.ts
+++ src/helpers/jwtHelpers.ts
@@ -1,20 +1,18 @@
 import jwt, { JwtPayload, Secret, SignOptions } from "jsonwebtoken";
 
-const generateToken = (payload: any, secret: Secret, expiresIn: string) => {
-  const options: SignOptions = {
-    algorithm: "HS256",
-    expiresIn,
-  };
-  const token = jwt.sign(payload, secret as string, options);
-
+const generateToken = (
+  payload: any,
+  secret: Secret,
+  expiresIn: string | number
+) => {
+  const options: SignOptions = { expiresIn: expiresIn as any };
+  const token = jwt.sign(payload, secret, options);
   return token;
 };
 
 const verifyToken = (token: string, secret: Secret) => {
   const tokenWithoutQuotes = token.replace(/^"|"$/g, "");
-
   const verifiedUser = jwt.verify(tokenWithoutQuotes, secret) as JwtPayload;
-
   return verifiedUser;
 };
 
